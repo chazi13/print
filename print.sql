5649068c526d578d1d8c981fe3bb2f531be7eea3
chazi13 <m.chairul669@gmail.com>
2019-06-18 19:27:47 +0700

    Database

--- print.sql
+++ print.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 17, 2019 at 01:02 PM
+-- Generation Time: Jun 18, 2019 at 12:27 PM
 -- Server version: 5.6.37
 -- PHP Version: 7.1.8
 
@@ -40,6 +40,30 @@ CREATE TABLE IF NOT EXISTS `admin` (
 
 INSERT INTO `admin` (`id_admin`, `nama`, `foto`, `username`, `password`) VALUES
 (1, 'Administrator', '', 'admin', 'cbbb842e45cfca6aed3c902a59fd8a10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE IF NOT EXISTS `detail_transaksi` (
+  `id_detail` int(11) NOT NULL,
+  `jml_pesan` int(11) NOT NULL,
+  `desain` tinyint(1) NOT NULL,
+  `file` varchar(100) NOT NULL,
+  `catatan` varchar(255) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_detail`, `jml_pesan`, `desain`, `file`, `catatan`, `id_produk`, `id_transaksi`) VALUES
+(1, 2, 1, 'upload/adi_purnomo/1560844817.png', 'papapapapa', 1, 1),
+(2, 1, 1, 'upload/adi_purnomo/1560844839.png', 'kakakakakak', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -81,6 +105,44 @@ INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `gambar`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `keranjang`
+--
+
+CREATE TABLE IF NOT EXISTS `keranjang` (
+  `id_keranjang` int(11) NOT NULL,
+  `jml_pesan` int(11) NOT NULL,
+  `desain` tinyint(1) NOT NULL DEFAULT '0',
+  `file` varchar(100) NOT NULL,
+  `catatan` varchar(255) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konfirmasi_pembayaran`
+--
+
+CREATE TABLE IF NOT EXISTS `konfirmasi_pembayaran` (
+  `id_konfirmasi` int(11) NOT NULL,
+  `nama_bank` varchar(20) NOT NULL,
+  `nama_akun` varchar(50) NOT NULL,
+  `no_rek` varchar(20) NOT NULL,
+  `file_bukti` varchar(100) NOT NULL,
+  `id_transaksi` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `konfirmasi_pembayaran`
+--
+
+INSERT INTO `konfirmasi_pembayaran` (`id_konfirmasi`, `nama_bank`, `nama_akun`, `no_rek`, `file_bukti`, `id_transaksi`) VALUES
+(1, 'Mandiri', 'Adi Purnomo', '0124151212612', 'upload/bukti/pembayaran/1560852370.png', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `produk`
 --
 
@@ -91,14 +153,15 @@ CREATE TABLE IF NOT EXISTS `produk` (
   `harga` decimal(10,0) NOT NULL,
   `keterangan` text NOT NULL,
   `id_kategori` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `produk`
 --
 
 INSERT INTO `produk` (`id_produk`, `nama_produk`, `gambar`, `harga`, `keterangan`, `id_kategori`) VALUES
-(1, 'PAKET Roll Up Banner 60 x 160 (cm)', '../assets/img/produk/Display/1558668244.png.png', '212900', 'Albatros + Laminating + Display Stand Roll Up', 7);
+(1, 'PAKET Roll Up Banner 60 x 160 (cm)', 'assets/img/produk/Display/1558668244.png.png', '212900', 'Albatros + Laminating + Display Stand Roll Up', 7),
+(2, 'PAKET Pop Up Table', 'assets/img/produk/Display/1558668141.png.png', '2070000', 'Sticker + Laminating + Pop Up Table', 7);
 
 -- --------------------------------------------------------
 
@@ -128,6 +191,31 @@ CREATE TABLE IF NOT EXISTS `tentang` (
   `id` int(1) NOT NULL,
   `deskripsi` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE IF NOT EXISTS `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `kode_transaksi` varchar(10) NOT NULL,
+  `tgl` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `metode_pengiriman` varchar(10) NOT NULL,
+  `resi` varchar(20) DEFAULT NULL,
+  `bukti_kirim` varchar(100) DEFAULT NULL,
+  `total` decimal(10,0) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '0',
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `kode_transaksi`, `tgl`, `metode_pengiriman`, `resi`, `bukti_kirim`, `total`, `status`, `id_user`) VALUES
+(1, '#TRK-0001', '2019-06-18 15:02:32', 'jne_reg', '21093530075131', 'upload/bukti/kirim/1560855563.png', '2624800', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -164,6 +252,12 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id_detail`);
+
+--
 -- Indexes for table `fitur`
 --
 ALTER TABLE `fitur`
@@ -174,6 +268,18 @@ ALTER TABLE `fitur`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id_keranjang`);
+
+--
+-- Indexes for table `konfirmasi_pembayaran`
+--
+ALTER TABLE `konfirmasi_pembayaran`
+  ADD PRIMARY KEY (`id_konfirmasi`);
 
 --
 -- Indexes for table `produk`
@@ -194,6 +300,12 @@ ALTER TABLE `tentang`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -209,6 +321,11 @@ ALTER TABLE `user`
 ALTER TABLE `admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `fitur`
 --
 ALTER TABLE `fitur`
@@ -219,10 +336,25 @@ ALTER TABLE `fitur`
 ALTER TABLE `kategori`
   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
+-- AUTO_INCREMENT for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `konfirmasi_pembayaran`
+--
+ALTER TABLE `konfirmasi_pembayaran`
+  MODIFY `id_konfirmasi` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `user`
 --
