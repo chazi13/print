47fbd1c475e5580cf525e4b57a78f426daf47941
chazi13 <m.chairul669@gmail.com>
2019-06-27 22:15:05 +0700

    Fix

--- admin/sistem/hapus_user.php
+++ admin/sistem/hapus_user.php
@@ -5,11 +5,12 @@ $id_user = $_GET['id'];
 
 $query1 = mysqli_query($koneksi, "DELETE FROM user WHERE id_user = '$id_user'");
 $query2 = mysqli_query($koneksi, "DELETE FROM pesanan WHERE id_user = '$id_user'");
+$query2 = mysqli_query($koneksi, "DELETE FROM testimoni WHERE id_user = '$id_user'");
 
 if ($query1 && $query2) {
     $_SESSION['pesan'] = [
         'status' => 'success',
-        'isi' => 'User berhasil hapus'
+        'isi' => 'User berhasil dihapus'
     ];
 } else {
     $_SESSION['pesan'] = [
